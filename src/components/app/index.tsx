858fb1c7dd28deefb51076f11e56944cccd10541
Viacheslav Voytovich <voytovich.slava@gmail.com>
2020-10-31 22:31:35 +0700

    move initial zone names to the state

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 import Timezones from '../timezones';
 
-function App(): JSX.Element {
-  return (
-    <div id="layout">
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <Timezones
-        zoneNames={[
-          'Asia/Krasnoyarsk',
-          'Europe/Moscow',
-        ]}
-      />
-    </div>
-  );
+interface IAppState {
+  zoneNames: string[],
+}
+
+class App extends Component<unknown, IAppState> {
+  constructor(props: unknown) {
+    super(props);
+
+    this.state = {
+      zoneNames: [
+        'Asia/Krasnoyarsk',
+        'Europe/Moscow',
+      ],
+    };
+  }
+
+  render(): JSX.Element {
+    const { zoneNames } = this.state;
+
+    return (
+      <div id="layout">
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <Timezones
+          zoneNames={zoneNames}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
